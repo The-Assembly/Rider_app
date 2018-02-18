34a3e3b63a3223977c72aa652871c3156b2606af
The-Assembly <32713072+The-Assembly@users.noreply.github.com>
2018-02-18 18:31:33 +0400

    Add files via upload

---
+++ Apps/server.js
@@ -0,0 +1,45 @@
+  var express = require('express');
+    var bodyParser = require('body-parser');
+    var Pusher = require('pusher');
+
+    var app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    var pusher = new Pusher({ // connect to pusher
+      appId: process.env.APP_ID, 
+      key: process.env.APP_KEY, 
+      secret:  process.env.APP_SECRET,
+      cluster: process.env.APP_CLUSTER, 
+    });
+
+    app.get('/', function(req, res){ // for testing if the server is running
+      res.send('all is well...');
+    });
+
+    // for authenticating users
+    app.get("/pusher/auth", function(req, res) {
+      var query = req.query;
+      var socketId = query.socket_id;
+      var channel = query.channel_name;
+      var callback = query.callback;
+
+      var auth = JSON.stringify(pusher.authenticate(socketId, channel));
+      var cb = callback.replace(/\"/g,"") + "(" + auth + ");";
+
+      res.set({
+        "Content-Type": "application/javascript"
+      });
+
+      res.send(cb);
+    });
+
+    app.post('/pusher/auth', function(req, res) {
+      var socketId = req.body.socket_id;
+      var channel = req.body.channel_name;
+      var auth = pusher.authenticate(socketId, channel);
+      res.send(auth);
+    });
+
+    var port = process.env.PORT || 5000;
+    app.listen(port);
